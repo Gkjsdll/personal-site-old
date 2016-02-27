6092005abbb4b9d79163527cc6c312a06b2fe554
Zack Winchell <zackwinchell@gmail.com>
2016-02-27 11:37:47 -0800

    implemented basic angular ui router functionality

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 "use strict";
 
-console.log(`Javascript loaded`);
+console.log("main.js");
+
+var app = angular.module("mainApp", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("home", {url: "/", templateUrl: "./partials/home.html", controller: "homeCtrl"})
+    .state("blog", {url: "/blog", templateUrl: "./partials/blog.html", controller: "blogCtrl"})
+
+    $urlRouterProvider.otherwise("/");
+});
+
+app.controller("homeCtrl", function($http, $scope) {
+  console.log("homeCtrl");
+  $http.get("/projects.json")
+    .then((res) => {
+      $scope.projects = res.data;
+    });
+
+    $scope.switchImg = function($event, imagePath) {
+      $event.target.parentElement.style.backgroundImage = `url("images/${imagePath}")`;
+    }
+});
+
+app.controller("blogCtrl", function() {
+  console.log("blogCtrl");
+});
